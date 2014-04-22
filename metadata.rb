5586c016b1f49bdd25194ad44d4d31846aa983d6
Stuart Philp <stuart.philp@polarmobile.com>
2014-04-22 14:45:42 -0400

    dev version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "A library cookbook for extracting archive files"
 long_description "A library cookbook for extracting archive files"
-version          "0.3.0"
+version          "0.3.1dev"
 
-supports "ubuntu"
+supports "centos"
